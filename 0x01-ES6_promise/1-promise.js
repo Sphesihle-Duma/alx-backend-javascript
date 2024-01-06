68e9372a77b8d6331215074fe71fa125c64c26a7
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-06 05:56:34 -0800

    Returning the correct object if a promise is resolved

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function (success) {
   if (success) {
-    return Promise.resolve({ status: 200, body: success });
+    return Promise.resolve({ status: 200, body: 'Success' });
   }
 
   return Promise.reject(new Error('The fake API is not working currently'));
