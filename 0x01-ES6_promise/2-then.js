164c5de476f37c42ce03530065b028e03ea5c999
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-06 06:17:42 -0800

    Added 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,5 @@
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => ({ status: 200, body: 'success' }))
+    .catch(() => {});
+  console.log('Got a response from the API');
+}
