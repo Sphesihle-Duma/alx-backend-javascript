1962e1ca526b3643f108b2678ddfc50ead1c8664
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-08 12:43:36 -0800

    Returning array of students ids fron array of students

---
+++ 0x03-ES6_data_manipulation/1-main.js
@@ -0,0 +1,4 @@
+import getListStudentIds from './1-get_list_student_ids'
+import getListStudents from './0-get_list_students'
+console.log(getListStudentIds('Hello'));
+console.log(getListStudentIds(getListStudents()))
