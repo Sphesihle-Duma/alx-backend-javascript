fab4470abb1242ba7e05c951aef6ceed01898235
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-09 12:52:22 -0800

    Added 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,6 @@
+function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  return filteredValues.join('-').replace(new RegExp(`^${startString}`), '');
+}
+
+export default cleanSet;
