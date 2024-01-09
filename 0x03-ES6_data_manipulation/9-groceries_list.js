963f9c377d6954ba4d91e6e5f2d473f2005e2cd9
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-09 13:10:03 -0800

    Returning a map of groceries

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+function groceriesList() {
+  const groceriesMap = new Map([
+    ['Apples', 5],
+    ['Bananas', 3],
+    ['Tomatoes', 2],
+    ['Potatoes', 10],
+    ['Onions', 4],
+  ]);
+
+  return groceriesMap;
+}
+
+export default groceriesList;
