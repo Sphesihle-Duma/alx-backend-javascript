c5214c15f586be6ba3ffda9fffb134ef63a0018c
Sphesihle-Duma <dumasphesihle22@gmail.com>
2024-01-14 05:35:21 -0800

    using a value parameter

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -10,8 +10,8 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  set floors(floors) {
-    this._floors = floors;
+  set floors(value) {
+    this._floors = value;
   }
 
   evacuationWarningMessage() {
