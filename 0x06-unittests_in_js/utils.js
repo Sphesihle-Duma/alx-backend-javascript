94cd93769467abd3a765eefdcfaeebf2d5a01421
Your Name <you@example.com>
2024-03-01 12:11:01 -0800

    updating the util module

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,20 +1,16 @@
 const Utils = {
- calculateNumber: (type, a, b) => {
-    if (typeof type === 'string') {
-      if (type === 'DIVIDE') {
-        if (Math.round(b) === 0) {
-          return 'Error';
-        } else {
-          return Math.round(a) / Math.round(b);
-        }
-      } else if (type === 'SUM') {
-        return Math.round(a) + Math.round(b);
-      } else if (type === 'SUBTRACT') {
-        return Math.round(a) - Math.round(b);
-      }
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
     }
-    return 'Error';
-  }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+    }
+    return 0;
+  },
 };
 
 module.exports = Utils;
